f538e1ae81aa17afb0a82f8b33a1ce3d4adf43c4
小飼慎一 <kogai0121@gmail.com>
2016-09-14 23:15:38 +0900

    2_3

---
+++ src/exercise_2_3.scala
@@ -0,0 +1,12 @@
+object exercise_2_3 {
+  def curry[A, B, C] (f: (A, B) => C): A => B => C = {
+    return a => b => f(a, b)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val c = (a: Int, b: Int) => a.toString() + b.toString()
+    val curried = curry(c)
+    println(curryable(1, 2))
+    println(curried(1)(2))
+  }
+}
